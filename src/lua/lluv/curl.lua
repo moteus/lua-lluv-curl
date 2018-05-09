e690f3d6f521dc2bba79ea0b3e7296e5e0b7c3cf
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2018-05-09 14:23:41 +0300

    Release 0.1.1

--- src/lua/lluv/curl.lua
+++ src/lua/lluv/curl.lua
@@ -1,12 +1,24 @@
+------------------------------------------------------------------
+--
+--  Author: Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Copyright (C) 2017-2018 Alexey Melnichuk <alexeymelnichuck@gmail.com>
+--
+--  Licensed according to the included 'LICENSE' document
+--
+--  This file is part of lua-lluv-curl library.
+--
+------------------------------------------------------------------
+
 local curl         = require "cURL.safe"
 local uv           = require "lluv"
 local ut           = require "lluv.utils"
 local EventEmitter = require "EventEmitter".EventEmitter
 
-local _VERSION   = "0.1.1-dev"
+local _VERSION   = "0.1.1"
 local _NAME      = "lluv-curl"
 local _LICENSE   = "MIT"
-local _COPYRIGHT = "Copyright (c) 2017 Alexey Melnichuk"
+local _COPYRIGHT = "Copyright (c) 2017-2018 Alexey Melnichuk"
 
 local function super(class, self, method, ...)
   if class.__base and class.__base[method] then
